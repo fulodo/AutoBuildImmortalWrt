73af4a0e0397707328dd80d34bbbf8d8b0ad5eff
fulodo <149771436+fulodo@users.noreply.github.com>
2025-03-13 10:23:52 +0800

    Update build.sh

--- x86-64/build.sh
+++ x86-64/build.sh
@@ -44,6 +44,7 @@ PACKAGES="$PACKAGES luci-i18n-cifs-mount-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-upnp-zh-cn"
 PACKAGES="$PACKAGES luci-i18n-statistics-zh-cn"
 PACKAGES="$PACKAGES qrencode"
+PACKAGES="$PACKAGES luci-i18n-wechatpush-zh-cn"
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
 PACKAGES="$PACKAGES script-utils"
